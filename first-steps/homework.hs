8cbb62cde007c04a922be1dff380d473bab3fc2c
Pamela Mori <pam.cdm@gmail.com>
2015-10-31 21:42:56 -0200

    Sorted function with reverse

--- first-steps/homework.hs
+++ first-steps/homework.hs
@@ -3,3 +3,18 @@ factorial n = product [1..n]
 avarage ns = sum ns `div` length ns
 
 crazyFunction word = head [length word]
+
+n = a `div` length xs 
+    where 
+      a = 10
+      xs = [1,2,3,4,5]
+
+last xs = head (drop (length xs -1 ) xs) 
+
+myProduct [] = 1 
+myProduct (x : xs) = x * myProduct xs
+
+reverseSorted [] = []
+reverseSorted (x : xs) =  reverseSorted larger ++ [x] ++ reverseSorted smaller
+  where smaller = [a | a <- xs, a < x]
+        larger = [b | b <- xs, b >= x]
