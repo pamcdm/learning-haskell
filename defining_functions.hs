45e43ce565d9acc579315a7d17c963ee559a533f
Pamela Mori <pam.cdm@gmail.com>
2015-11-11 23:10:09 -0200

    Trying out defining functions

--- defining_functions.hs
+++ defining_functions.hs
@@ -34,3 +34,23 @@ safeTail8
     	[ ] -> [ ]
     	(_ : xs) -> xs
 
+--import Prelude hiding ((||))
+
+--False || False = False
+--_ || _ = True
+
+--False || False = False
+--_ || _ = True
+
+
+--import Prelude hiding ((&&))
+
+--True && True = True
+--_ && _ = False
+
+a && b = if a then if b then True else False else False
+
+remove n xs = take n xs ++ drop (n + 1) xs
+
+funct :: Int -> [a] -> [a]
+funct x xs = take (x + 1) xs ++ drop x xs
