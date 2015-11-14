b89f39e112f284b566c7e0003e7ff484644e078c
Pamela Mori <pam.cdm@gmail.com>
2015-11-14 17:21:57 -0200

    Add function the select the nth element

--- recursion.hs
+++ recursion.hs
@@ -3,3 +3,8 @@
 replicateList :: Int -> a -> [a]
 replicateList 0 = []
 replicateList x = x : replicateList (x - 1)
+
+-- select the nth element of a list
+selectNElement :: [a] -> Int -> a
+selectNElement [x] = x
+selectNElement xs = tail (selectNElement -1)
