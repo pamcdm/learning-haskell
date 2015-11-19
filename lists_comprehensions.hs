d1f74100588be345907802be0f0d21073aa15131
Pamela Mori <pam.cdm@gmail.com>
2015-11-18 22:54:25 -0200

    Playing with lists and section guards

--- lists_comprehensions.hs
+++ lists_comprehensions.hs
@@ -1,6 +1,28 @@
-pairs :: [a] -> [a] -> [(a,a)]
+-- List comprenhensions 
+-- Construct new lists from old list with generators
+
+concat :: [[a]] -> [a]
+concat xss = [x | xs <- xss, x <- xs]
+
+-- Using guards
+
+generateEvenList :: Integer -> [Integer]
+generateEvenList n = [x | x <- [1..n], even x]
+
+whereAreMyFactors :: Int -> [Int]
+whereAreMyFactors n = 
+	[x | x <- [1..n], n `mod` x == 0]
+
+areYouPrime :: Int -> Bool
+areYouPrime n = whereAreMyFactors n == [1,n]
+
+-- Using zip to define pairs
+
+pairs :: [a] -> [(a,a)]
 pairs xs = zip xs (tail xs)
 
-sorted :: Ord a -> [a] -> Bool
+-- And then using pairs to define sorted 
+
+sorted :: Ord a => [a] -> Bool
 sorted xs =
 	and [x <= y | (x,y) <- pairs xs]
