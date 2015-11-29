bfbddf94ebe9654efcdee5453668b57abd1f2a30
Pamela Mori <pam.cdm@gmail.com>
2015-11-28 23:40:08 -0200

    Exercises with recursion

--- recursion.hs
+++ recursion.hs
@@ -1,10 +1,83 @@
 -- produce a list with n identical elements
 
-replicateList :: Int -> a -> [a]
-replicateList 0 = []
-replicateList x = x : replicateList (x - 1)
+--replicateList :: Int -> a -> [a]
+--replicateList 0 = []
+--replicateList x = x : replicateList (x - 1)
 
--- select the nth element of a list
-selectNElement :: [a] -> Int -> a
-selectNElement [x] = x
-selectNElement xs = tail (selectNElement -1)
+---- select the nth element of a list
+--selectNElement :: [a] -> Int -> a
+--selectNElement [x] = x
+--selectNElement xs = tail (selectNElement -1)
+
+-- HomeWork exercises
+
+-- Exercise 4
+-- Valid implemantions of and funciton
+
+and1 :: [Bool] -> Bool
+and1 [] = True
+and1 (b: bs) = b && and1 bs
+
+and2 :: [Bool] -> Bool
+and2 [] = True
+and2 (b : bs)
+  | b = and2 bs
+  | otherwise = False
+
+and3 :: [Bool] -> Bool
+and3 [] = True
+and3 (b : bs)
+  | b == False = False
+  | otherwise = and3 bs
+
+and5 :: [Bool] -> Bool
+and5 [] = True
+and5 (b : bs) = and5 bs && b
+
+-- Exercise 5
+-- Valid implementations of concat function 
+
+concat2 :: [[a]] -> [a]
+concat2 [] = []
+concat2 (xs : xss) = xs ++ concat2 xss
+
+concat4 :: [[a]] -> [a]
+concat4 [[]] = []
+concat4 (xs : xss) = xs ++ concat4 xss
+
+-- Exercise 8
+
+hasElem :: Eq a => a -> [a] -> Bool
+hasElem _ [] = False
+hasElem x (y : ys)
+  | x == y = True
+  | otherwise = hasElem x ys
+
+-- Exercise 9 
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y : ys)
+  = if x <= y then x : merge xs (y : ys) else y : merge (x : xs) ys	
+
+-- Exercise 10
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort ys) (msort zs)
+  where (ys, zs) = halve xs
+
+
+
+
+
+
+
+
+
+
